b0b48b659b9a54886b8346dba88aaecd94b368a8
Wendy Smoak <wsmoak@gmail.com>
2015-07-23 08:51:38 -0400

    Initial solution for Chop number guess

--- pe/ch6/chop.exs
+++ pe/ch6/chop.exs
@@ -8,11 +8,22 @@ defmodule Chop do
 
   # Base case.  The guess is correct and we terminate.
    def _guess( actual, _, current_guess = actual ) do
+    IO.puts "Is it #{current_guess}"
     IO.puts current_guess
   end
 
-  def _guess( actual, first..last, current_guess ) do
+  # The guess wasn't correct
+
+  def _guess( actual, first..last, current_guess ) when current_guess > actual do
     IO.puts "Is it #{current_guess}"
+    # Look in the lower half of the range
+    _guess( actual, first..current_guess, div( first+current_guess, 2 ) )
+  end
+
+  def _guess( actual, first..last, current_guess) when current_guess < actual do
+    IO.puts "Is it #{current_guess}"
+    # Look in the upper half of the range
+    _guess( actual, current_guess..last, div( current_guess+last, 2 ) )
   end
 
 end
